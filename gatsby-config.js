8b86642a6a3469b13a06c12e1fb673b778d18cc6
Colin Bull <colin.bull@digital.education.gov.uk>
2021-03-08 16:34:40 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,26 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-transformer-remark",
+    {   resolve: "gatsby-transformer-remark",
+        options: {
+            plugins: [
+                {
+                    resolve: `gatsby-remark-classes`,
+                    options: {
+                        classMap: {
+                            "heading[depth=1]": "govuk-heading-xl",
+                            "heading[depth=2]": "govuk-heading-l",
+                            "heading[depth=3]": "govuk-heading-m",
+                            "heading[depth=4]": "govuk-heading-s",
+                            "paragraph": "govuk-body",
+                            "list[ordered=false]": "govuk-list govuk-list--bullet",
+                            "list[ordered=true]": "govuk-list govuk-list--number"
+                        }
+                    }
+                }
+            ]
+        }
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -12,6 +31,14 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "markdown",
+        path: "./src/pages/markdown",
+      },
+      __key: "markdown",
     },
   ],
 };
